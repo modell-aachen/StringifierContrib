ea6b09b349aabd0084635b5fceeaa6c2aac1dc39
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-03-25 14:51:12 +0000

    Item8765: Replace use base with use; our @ISA;
    git-svn-id: http://svn.foswiki.org/trunk/StringifierContrib@6912 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- test/unit/StringifierContrib/StringifierContribSuite.pm
+++ test/unit/StringifierContrib/StringifierContribSuite.pm
@@ -1,5 +1,6 @@
 package StringifierContribSuite;
-use base qw(Unit::TestSuite);
+use Unit::TestSuite;
+our @ISA = qw( Unit::TestSuite );
 
 sub include_tests {
   qw(  StringifyBaseTest Doc_antiwordTests Doc_wvTests DocxTests Doc_abiwordTests XlsTests PdfTests TxtTests HtmlTests PptTests PptxTests XlsxTests);
