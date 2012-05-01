aa77f627df5fc046a3e11c65dae8653dd80e38e3
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-01 14:07:26 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule
    git-svn-id: http://svn.foswiki.org/trunk/StringifierContrib@14680 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- tools/xls2txt.pl
+++ tools/xls2txt.pl
@@ -4,61 +4,62 @@
 # it will not take down all of the foswiki process. See http://foswiki.org/Tasks/ItemXXX
 
 use strict;
-use Spreadsheet::ParseExcel ();
-use Spreadsheet::ParseExcel::FmtUnicode (); 
+use Spreadsheet::ParseExcel             ();
+use Spreadsheet::ParseExcel::FmtUnicode ();
 use Error qw(:try);
 use Encode ();
 
 my $file = $ARGV[0];
 
 unless ($file) {
-  print STDERR "usage: xls2txt <file>\n";
-  exit 1;
+    print STDERR "usage: xls2txt <file>\n";
+    exit 1;
 }
 
-unless (-e $file) {
-  print STDERR "file not found: $file\n";
-  exit 1;
+unless ( -e $file ) {
+    print STDERR "file not found: $file\n";
+    exit 1;
 }
 
 my $format = Spreadsheet::ParseExcel::FmtDefault->new();
 my $book;
 
 try {
-  $book = Spreadsheet::ParseExcel::Workbook->Parse($file, $format);
+    $book = Spreadsheet::ParseExcel::Workbook->Parse( $file, $format );
 }
 catch Error with {
 
-  # file not opened, possibly passworded
-  print STDERR shift->{-text} . "\n";
-  exit 1;
+    # file not opened, possibly passworded
+    print STDERR shift->{-text} . "\n";
+    exit 1;
 };
 
 return '' unless $book;
 
 my $text = '';
 
-foreach my $sheet (@{ $book->{Worksheet} }) {
-  last if !defined $sheet->{MaxRow};
-  foreach my $row ($sheet->{MinRow} .. $sheet->{MaxRow}) {
-    foreach my $col ($sheet->{MinCol} .. $sheet->{MaxCol}) {
-      my $cell = $sheet->{Cells}[$row][$col];
-      if ($cell) {
-        my $cell_text;
-        if ($cell->{Type} eq "Numeric") {
-          $cell_text = $cell->{Val};
-        } else {
-          $cell_text = $cell->Value;
-        }
-        next if ($cell_text eq "");
+foreach my $sheet ( @{ $book->{Worksheet} } ) {
+    last if !defined $sheet->{MaxRow};
+    foreach my $row ( $sheet->{MinRow} .. $sheet->{MaxRow} ) {
+        foreach my $col ( $sheet->{MinCol} .. $sheet->{MaxCol} ) {
+            my $cell = $sheet->{Cells}[$row][$col];
+            if ($cell) {
+                my $cell_text;
+                if ( $cell->{Type} eq "Numeric" ) {
+                    $cell_text = $cell->{Val};
+                }
+                else {
+                    $cell_text = $cell->Value;
+                }
+                next if ( $cell_text eq "" );
 
-        $text .= $cell_text;
-      }
-      $text .= " ";
+                $text .= $cell_text;
+            }
+            $text .= " ";
+        }
+        $text .= "\n";
     }
     $text .= "\n";
-  }
-  $text .= "\n";
 }
-$text = Encode::encode('iso-8859-1', $text, 0) if utf8::is_utf8($text);
+$text = Encode::encode( 'iso-8859-1', $text, 0 ) if utf8::is_utf8($text);
 print $text;
